7b047d063efb3eccabc75e08e8a70be3309d5dd6
Mijovic18 <98288815+Mijovic18@users.noreply.github.com>
2022-02-26 15:18:19 +0100

    Add files via upload

---
+++ funkcije.js
@@ -0,0 +1,139 @@
+function zamaglinaziv1() {
+    document.getElementById("ajdi1").style.opacity = 0.5;
+}
+function odmaglinaziv1() {
+    document.getElementById("ajdi1").style.opacity = 1;
+}
+
+function zamaglinaziv2() {
+    document.getElementById("ajdi2").style.opacity = 0.5;
+}
+function odmaglinaziv2() {
+    document.getElementById("ajdi2").style.opacity = 1;
+}
+
+function zamaglinaziv3() {
+    document.getElementById("ajdi3").style.opacity = 0.5;
+}
+function odmaglinaziv3() {
+    document.getElementById("ajdi3").style.opacity = 1;
+}
+
+function zamaglinaziv4() {
+    document.getElementById("ajdi4").style.opacity = 0.5;
+}
+function odmaglinaziv4() {
+    document.getElementById("ajdi4").style.opacity = 1;
+}
+
+function zamaglinaziv5() {
+    document.getElementById("ajdi5").style.opacity = 0.5;
+}
+function odmaglinaziv5() {
+    document.getElementById("ajdi5").style.opacity = 1;
+}
+
+function zamaglinaziv6() {
+    document.getElementById("ajdi6").style.opacity = 0.5;
+}
+function odmaglinaziv6() {
+    document.getElementById("ajdi6").style.opacity = 1;
+}
+
+function zamaglinaziv7() {
+    document.getElementById("ajdi7").style.opacity = 0.5;
+}
+function odmaglinaziv7() {
+    document.getElementById("ajdi7").style.opacity = 1;
+}
+
+function zamaglinaziv8() {
+    document.getElementById("ajdi8").style.opacity = 0.5;
+}
+function odmaglinaziv8() {
+    document.getElementById("ajdi8").style.opacity = 1;
+}
+
+function zamaglinaziv9() {
+    document.getElementById("ajdi9").style.opacity = 0.5;
+}
+function odmaglinaziv9() {
+    document.getElementById("ajdi9").style.opacity = 1;
+}
+
+function zamaglinaziv10() {
+    document.getElementById("ajdi10").style.opacity = 0.5;
+}
+function odmaglinaziv10() {
+    document.getElementById("ajdi10").style.opacity = 1;
+}
+
+function zamaglinaziv11() {
+    document.getElementById("ajdi11").style.opacity = 0.5;
+}
+function odmaglinaziv11() {
+    document.getElementById("ajdi11").style.opacity = 1;
+}
+
+function zamaglinaziv12() {
+    document.getElementById("ajdi12").style.opacity = 0.5;
+}
+function odmaglinaziv12() {
+    document.getElementById("ajdi12").style.opacity = 1;
+}
+
+function zamaglinaziv13() {
+    document.getElementById("ajdi13").style.opacity = 0.5;
+}
+function odmaglinaziv13() {
+    document.getElementById("ajdi13").style.opacity = 1;
+}
+
+function zamaglinaziv14() {
+    document.getElementById("ajdi14").style.opacity = 0.5;
+}
+function odmaglinaziv14() {
+    document.getElementById("ajdi14").style.opacity = 1;
+}
+function galerijaopen(){
+    window.location.href = 'zadaci/1_ocena/FotoGalerijaA2/FotoGalerijaA2/index.html';
+}
+function prognozaopen() {
+    window.location.href = 'zadaci/2_ocena/VremenskaPrognoza/VremenskaPrognoza/index.html';
+}
+function kalkulatoropen() {
+    window.location.href = 'zadaci/3_ocena/Kalkulator/Kalkulator/index.html';
+}
+function srbijaopen() {
+    window.location.href = 'zadaci/4_ocena/Srbija/Srbija/index.html';
+}
+function numeroloskoopen() {
+    window.location.href = 'zadaci/1_ocena/NumeroloskoSlaganjeA11/NumeroloskoSlaganjeA11/index.html';
+}
+function muzickiopen() {
+    window.location.href = 'zadaci/2_ocena/MuzickiInstrumenti/MuzickiInstrumenti/index.html';
+}
+function messiopen() {
+    window.location.href = 'zadaci/3_ocena/Messi/Messi/index.html';
+}
+function srbijasusediopen() {
+    window.location.href = 'zadaci/4_ocena/SrbijaSusedi/SrbijaSusedi/index.html';
+}
+function sabiranjeopen() {
+    window.location.href = 'zadaci/1_ocena/SabiranjeDvaBroja/SabiranjeDvaBroja/index.html';
+}
+function karteopen() {
+    window.location.href = 'zadaci/2_ocena/AutobuskeKarteA5/AutobuskeKarteA5/index.html';
+}
+function djokovicopen() {
+    window.location.href = 'zadaci/3_ocena/Djokovic/Djokovic/index.html';
+}
+function automobiliopen() {
+    window.location.href = 'zadaci/4_ocena/SlajderAutomobili/SlajderAutomobili/index.html';
+}
+function sijalicaopen() {
+    window.location.href = 'zadaci/1_ocena/Sijalica/Sijalica/index.html';
+}
+function skolaopen() {
+    window.location.href = 'zadaci/4_ocena/SlajderA20/SlajderA20/index.html';
+}
